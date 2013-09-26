d97449fb56723dc7759c514f9797276b36c67069
kimyee <kimyee.16@gmail.com>
2013-09-26 02:47:23 -0400

    Complete your top choices.

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,6 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
+var places = ["Neverland", "NeverNeverLand", "CandyLand", "ColorPlaces"];
+console.log("My #1 choice is " + places[0] + ". My 2nd choice is " + places[1] + ". My 3rd choice is " + places[2] + ".");
 
