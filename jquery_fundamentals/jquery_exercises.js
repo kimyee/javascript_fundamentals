824471ee790aca060f2db3864a02f5bfb21d02f3
kimyee <kimyee.16@gmail.com>
2013-09-29 16:25:57 -0400

    Complete Change Background

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -5,6 +5,10 @@
 // Change the background color of '#target' by script. 
 // Hint: you can use .css
 
+#target{
+	background-color: green 
+};
+
 // EXERCISE: Change Parent
 
 // Change the text in the span, a child of "#target"
